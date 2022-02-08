489450f96eef55673c8998d540fb285b2127b801
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-02-08 09:47:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let currentScore = 0;
 let activePlayer = 0;
 const scores = [0, 0];
+let playing = true;
 
 const player0Element = document.querySelector('.player--0');
 const player1Element = document.querySelector('.player--1');
@@ -19,24 +20,50 @@ score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
+
 //Dice roll implementation
 
 btnRoll.addEventListener('click', function () {
-  const diceNumber = Math.trunc(Math.random() * 6 + 1);
-  console.log(diceNumber);
+  if (playing) {
+    const diceNumber = Math.trunc(Math.random() * 6 + 1);
+    console.log(diceNumber);
 
-  diceElement.classList.remove('hidden');
-  diceElement.src = `dice-${diceNumber}.png`;
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${diceNumber}.png`;
 
-  if (diceNumber !== 1) {
-    currentScore += diceNumber;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0Element.classList.toggle('player--active');
-    player1Element.classList.toggle('player--active');
+    if (diceNumber !== 1) {
+      currentScore += diceNumber;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    }
+    switchPlayer();
   }
 });
